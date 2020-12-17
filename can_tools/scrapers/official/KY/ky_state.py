686bf9bb7a320bbf8599cdd321dde4db331b3d03
Edward Turner <edward.turnerr@gmail.com>
2020-12-17 12:44:57 -0500

    more or less done

--- can_tools/scrapers/official/KY/ky_state.py
+++ can_tools/scrapers/official/KY/ky_state.py
@@ -18,87 +18,20 @@ logger = logging.getLogger(__name__)
 
 
 class Kentucky(DatasetBase, StateDashboard):
-    start_date = "2019-04-29"
-    source = (
-        "https://www.mass.gov/info-details/"
-        "covid-19-response-reporting#covid-19-daily-dashboard-"
-    )
+    start_date = "2020-12-17"
+    # source = (
+    #     "https://www.mass.gov/info-details/"
+    #     "covid-19-response-reporting#covid-19-daily-dashboard-"
+    # )
     state_fips = int(us.states.lookup("Kentucky").fips)
-    has_location = False
+    has_location = True
+    location_type = "county"
 
-    def transform_date(self, date: pd.Timestamp) -> pd.Timestamp:
-        return date - pd.Timedelta(hours=11)
+    def fetch(self):
+        """
 
-    def _get_cases_deaths(self, zf: zipfile.ZipFile) -> pd.DataFrame:
-        with zf.open("County.csv") as csv_f:
-            df = pd.read_csv(csv_f, parse_dates=["Date"])
-
-        names = {"Date": "dt", "County": "county"}
-        cats = {
-            "Total Confirmed Cases": CMU(
-                category="cases", measurement="cumulative", unit="people"
-            ),
-            "Total Probable and Confirmed Deaths": CMU(
-                category="deaths", measurement="cumulative", unit="people"
-            ),
-        }
-        out = df.rename(columns=names).loc[:, list(names.values()) + list(cats.keys())]
-        melted = out.melt(id_vars=["dt", "county"], value_vars=list(cats.keys()))
-
-        return (
-            melted.drop_duplicates(subset=["dt", "county", "variable"], keep="first")
-            .pipe(self.extract_CMU, cats)
-            .drop(["variable"], axis=1)
-            .assign(value=lambda x: x["value"].fillna(-1).astype(int))
-        )
-
-    def _get_hospital_data(
-        self, zf: zipfile.ZipFile, date: pd.Timestamp
-    ) -> Optional[pd.DataFrame]:
-        fn = "HospCensusBedAvailable.xlsx"
-        if fn not in [x.filename for x in zf.filelist]:
-            print("The file HospCensusBedAvailable.xlsx could not be found, skipping")
-            return None
-        with zf.open(fn, "r") as f:
-            # NOTE: needed b/c python 3.6 doesn't have `.seek` method on
-            # zipfile objects and pandas expects it
-            content = io.BytesIO(f.read())
-            df = pd.read_excel(content, sheet_name="Hospital COVID Census")
-
-        hosp_col = [x for x in list(df) if "Including ICU" in x]
-        if len(hosp_col) != 1:
-            raise ValueError(
-                f"Could not find total hospital column from list: {list(df)}"
-            )
-
-        icu_col = [x for x in list(df) if "ICU Census" in x]
-        if len(icu_col) != 1:
-            raise ValueError(f"Could not find ICU column from list: {list(df)}")
-
-        cats = {
-            hosp_col[-1]: CMU(
-                category="hospital_beds_in_use_covid",
-                measurement="current",
-                unit="beds",
-            ),
-            icu_col[-1]: CMU(
-                category="icu_beds_in_use_covid", measurement="current", unit="beds"
-            ),
-        }
-
-        return (
-            df.groupby("Hospital County")[list(cats.keys())]
-            .sum()
-            .reset_index()
-            .rename(columns={"Hospital County": "county"})
-            .assign(dt=date)
-            .melt(id_vars=["dt", "county"])
-            .pipe(self.extract_CMU, cats)
-            .drop(["variable"], axis=1)
-        )
-
-    def get(self, date: str) -> pd.DataFrame:
-
+        :return:
+        """
         file = "https://chfs.ky.gov/agencies/dph/covid19/COVID19DailyReport.pdf"
         tables: List[pd.DataFrame] = tabula.read_pdf(file, pages="all", multiple_tables=True)
 
@@ -117,8 +50,6 @@ class Kentucky(DatasetBase, StateDashboard):
                 start = True
             else:
                 pass
-
-        dt = pd.to_datetime(date)
 
         def reducer(left_df: pd.DataFrame, right_df: pd.DataFrame) -> pd.DataFrame:
             if {"County", "New Cases"}.issubset(set(left_df.columns)):
@@ -150,13 +81,76 @@ class Kentucky(DatasetBase, StateDashboard):
                             left_df
                         ), ignore_index=True
                     ).T.reset_index().T.rename(
-                                columns={0: "County", 1: "New Cases", 2: "Percent"}
-                            ).reset_index(drop=True)
+                        columns={0: "County", 1: "New Cases", 2: "Percent"}
+                    ).reset_index(drop=True)
 
         df = reduce(lambda left, right: reducer(left, right), dfs)
 
-        df["last_updated"] = dt
+        df["last_updated"] = self.execution_dt
 
-        df["value"] = df["value"].astype(int)
-        df["vintage"] = self._retrieve_vintage()
         return df
+
+    def _fetch(self):
+        """
+
+        :return:
+        """
+        data = self.fetch()
+        success = self._store_clean(data)
+
+        return success
+
+    def normalize(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+
+        :param data:
+        :return:
+        """
+        # TODO The location must be transformed to the FIPS Code
+
+        data_df = data.rename(columns={
+            "County": "location",
+            "New Cases": "value"
+        })
+
+        # us.states.lookup("Kentucky").
+
+        data_df['vintage'] = self._retrieve_vintage()
+        data_df['sex'] = 'all'
+        data_df['gender'] = 'all'
+        data_df['race'] = 'all'
+        data_df['measurement'] = 'new'
+        data_df['unit'] = 'people'
+        data_df['category'] = 'cases'
+
+        # this forces numeric values
+        data_df["value"] = pd.to_numeric(data_df["value"])
+
+        cols_to_keep = [
+            "vintage",
+            "dt",
+            "location",
+            "category",
+            "measurement",
+            "unit",
+            "age",
+            "race",
+            "sex",
+            "value",
+        ]
+
+        return data_df[cols_to_keep]
+
+    def _normalize(self):
+        """
+
+        :return:
+        """
+        # Ingest the data
+        data: pd.DataFrame = self._read_clean()
+
+        # Clean data using `_normalize`
+        df = self.normalize(data)
+        success = self._store_clean(df)
+
+        return success
